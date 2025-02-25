6d0efe4d40e9090096f8d63a3807788aebf196b8
Benjamin Batte <benjaminbatte@gmail.com>
2025-02-25 13:26:01 -0700

    search policy

--- src/app/pages/search-policy/search-policy.component.ts
+++ src/app/pages/search-policy/search-policy.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { PolicyService } from '../../services/policy.service';
+import { AutoPolicy } from '../../models/auto-policy.model';
 
 @Component({
   selector: 'app-search-policy',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule], 
   templateUrl: './search-policy.component.html',
-  styleUrl: './search-policy.component.css'
+  styleUrls: ['./search-policy.component.css']
 })
 export class SearchPolicyComponent {
+  policies: AutoPolicy[] = [];
+  searchForm = new FormGroup({
+    policyNumber: new FormControl(''),
+    status: new FormControl(''),
+    policyType: new FormControl(''),
+    vehicleMake: new FormControl(''),
+    startDate: new FormControl(''),
+    endDate: new FormControl('')
+  });
 
+  constructor(private policyService: PolicyService) {}
+
+  searchPolicies() {
+    const queryParams: any = {};
+    Object.entries(this.searchForm.value).forEach(([key, value]) => {
+      if (value) queryParams[key] = value;
+    });
+
+    this.policyService.searchPolicies(queryParams).subscribe((data) => {
+      this.policies = data;
+    });
+  }
 }
